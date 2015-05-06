b16f923fbc05a9a5742a13746a6ab6b8a042d5c9
av <avieru@gmail.com>
2015-05-06 12:20:32 -0400

    adding new .gitignore, updating packages, adding identity bits, moving what was in dbcontext to applicationdbcontext to combine identity stuff with custom models

--- EliaStore/App_Start/BundleConfig.cs
+++ EliaStore/App_Start/BundleConfig.cs
@@ -11,7 +11,11 @@ namespace EliaStore
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
